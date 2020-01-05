b61aecda7d17dfae7562b9612fde16fdb0afd3ad
Ubuntu <ubuntu@ip-172-31-38-113.ec2.internal>
2020-01-05 05:06:39 +0000

    add update advertisment in login page

--- app.py
+++ app.py
@@ -39,37 +39,51 @@ def insert_user():
     mongo.db.user.insert_one({"userName":request.form.get('userName'), "userEmail": request.form.get('userEmail'), "userContact": request.form.get('userContact')} )
     return redirect(url_for('mysearch'))
 
+# Go to Form for user to add advertisment
 @app.route('/add_adv/<user_id>')
 def add_adv(user_id):
     return render_template("add_adv.html",user_id=user_id) 
 
+# Insert pet information to MongoDB database
 @app.route('/insert_adv/', methods=['POST'])
 def insert_adv():
     mongo.db.pet.insert_one({"user_id": ObjectId(request.form.get('userID')), "petName": request.form.get('petName'), "petCat": request.form.get('petCat'), "gender": request.form.get('gender'), "color": request.form.get('color'), "age": request.form.get('age'), "description": request.form.get('description')} )
     return redirect(url_for('mysearch'))
 
+# Go to the login page for user to log in their account
 @app.route('/login')
 def login():
     return render_template("login.html")
 
+# Retrieve the details of the user from MongoDB 
 @app.route('/mylogin', methods=['POST'])
 def mylogin():
     mykeyword = request.form.get('login')
     mydata = {"userName":  mykeyword}
     myuser=mongo.db.user.find(mydata)
     return render_template("mylogin.html", user=myuser)
-    
+
+# Update the user details to MongoDB
 @app.route('/update_user/', methods=['POST'])
 def update_user():
     query = {'_id':ObjectId(request.form.get('user_id'))}
     mongo.db.user.update(query, {"userName": request.form.get('myName'), "userEmail": request.form.get('myEmail'), "userContact": request.form.get('myContact')} )
     return redirect(url_for('mysearch'))
 
+# Search and display the advertisment posted by the user
 @app.route('/my_adv/<user_id>')
 def my_adv(user_id):
     mypet=mongo.db.pet.find({"user_id":  ObjectId(user_id)})
     return render_template("my_adv.html", pet=mypet)  
-    
+
+# Insert pet information to MongoDB database
+@app.route('/update_adv/', methods=['POST'])
+def update_adv():
+    query = {'_id':ObjectId(request.form.get('adv_id'))}
+    mongo.db.pet.update(query, {"petName": request.form.get('petName'), "petCat": request.form.get('petCat'), "gender": request.form.get('gender'), "color": request.form.get('color'), "age": request.form.get('age'), "description": request.form.get('description')} )
+    return redirect(url_for('mysearch'))
+
+# Delete the Advertisment
 @app.route('/delete_adv/<adv_id>')
 def delete_adv(adv_id):
     return render_template("deleteAdv.html",adv_id=adv_id) 
