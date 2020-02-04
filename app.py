9c42ab3107035eeec6d033b8aad398e282449b36
Ubuntu <ubuntu@ip-172-31-38-113.ec2.internal>
2020-02-04 17:08:09 +0000

    Add in the cimple validation for all forms

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
 
 #Import all the required libraries
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from werkzeug.utils import secure_filename
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
 
 
 # Connection to MongoDB
 app = Flask(__name__)
+app.secret_key = 'This_is_secret_key'
 app.config['UPLOAD_FOLDER'] = '/home/ubuntu/enivroment/uploads'
 app.config["MONGO_DBNAME"] = 'pet_adopt'
 app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
+
 mongo = PyMongo(app)
+
 
 @app.route('/')
 
@@ -23,16 +26,25 @@ def mysearch():
     return render_template("index.html", pet=mongo.db.pet.find(), user=list(mongo.db.user.find()))
 
 
+
 # Display the search Result
 @app.route('/showsearch', methods=['POST'])
 def showsearch():
+    
     mykeyword = request.form.get('searchPetCat')
     mydata = {"petCat":  mykeyword}
    
     mypet=mongo.db.pet.find(mydata)
     myuser=mongo.db.user.find()
-  
-    return render_template("searchResult.html", pet=mypet,user=list(myuser))  
+    
+    # Display the list of pets or No result found.
+    mypet1=mongo.db.pet.find_one(mydata)
+    if not mypet1: 
+        flash('The pets is not available. Please search for other pets')
+        return render_template("searchResult.html", pet=mypet,user=list(myuser))  
+    else:
+        return render_template("searchResult.html", pet=mypet,user=list(myuser))  
+
 
 
 # Form for user to sign up user account
@@ -41,17 +53,32 @@ def add_user():
     return render_template("add_user.html", pet=mongo.db.user.find())
 
 
-# Insert user information to MongoDB database
+
+# Insert user information to MongoDB database. Check if the username already exist.
 @app.route('/insert_user/', methods=['POST'])
 def insert_user():
-    mongo.db.user.insert_one({"userName":request.form.get('userName'), "userEmail": request.form.get('userEmail'), "userContact": request.form.get('userContact')} )
-    return redirect(url_for('mysearch'))
+    if request.method == 'POST':
+        users = mongo.db.user
+        existing_user = users.find_one({'userName' : request.form['userName']})
+        
+        if existing_user is None:
+            mongo.db.user.insert_one({"userName":request.form.get('userName'), "userEmail": request.form.get('userEmail'), "userContact": request.form.get('userContact')} )
+            myuser=mongo.db.user.find({"userName": request.form['userName']})
+            return render_template("mylogin.html", user=myuser)
+            
+        flash('USER NAME ALREADY EXIST! PLEASE REGISTER WITH A DIFFERENT USER NAME.')
+        return redirect(url_for('add_user'))
+        # return 'User Name already exist! Please click back and register with a different User Name.'
+    
+    return render_template("add_user.html", pet=mongo.db.user.find())
+
 
 
 # Form for user to add advertisment
 @app.route('/add_adv/<user_id>')
 def add_adv(user_id):
     return render_template("add_adv.html",user_id=user_id) 
+
 
 
 # Insert pet information to MongoDB database
@@ -61,34 +88,50 @@ def insert_adv():
     return redirect(url_for('mysearch'))
 
 
+
 # Go to the login page for user to log in their account
 @app.route('/login')
 def login():
     return render_template("login.html")
 
 
-# Retrieve the details of the user from MongoDB 
+
+# Retrieve the details of the user from MongoDB. Check if the user name is valid.
 @app.route('/mylogin', methods=['POST'])
 def mylogin():
-    mykeyword = request.form.get('login')
-    mydata = {"userName":  mykeyword}
-    myuser=mongo.db.user.find(mydata)
-    return render_template("mylogin.html", user=myuser)
-
-
+    myuser=mongo.db.user.find_one({"userName": request.form['login']})
+    
+    if myuser:
+        myuser=mongo.db.user.find({"userName": request.form['login']})
+        return render_template("mylogin.html", user=myuser)
+    
+    flash('INVALID USER NAME! PLEASE SIGN IN WITH A VALID USER NAME.')
+    return redirect(url_for('login'))
+   
+   
+   
 # Update the user details to MongoDB
 @app.route('/update_user/', methods=['POST'])
 def update_user():
     query = {'_id':ObjectId(request.form.get('user_id'))}
     mongo.db.user.update(query, {"userName": request.form.get('myName'), "userEmail": request.form.get('myEmail'), "userContact": request.form.get('myContact')} )
-    return redirect(url_for('mysearch'))
+    myuser=mongo.db.user.find({"userName": request.form['myName']})
+    return render_template("mylogin.html", user=myuser)
+
 
 
 # Search and display the advertisment posted by the user
 @app.route('/my_adv/<user_id>')
 def my_adv(user_id):
     mypet=mongo.db.pet.find({"user_id":  ObjectId(user_id)})
-    return render_template("my_adv.html", pet=mypet)  
+    
+    mypet1=mongo.db.pet.find_one({"user_id":  ObjectId(user_id)})
+    if not mypet1:
+        flash('You do not have any existing Advertisment. Please go back to add new Advertisment.')
+        return render_template("my_adv.html", pet=mypet) 
+    else:
+        return render_template("my_adv.html", pet=mypet)  
+
 
 
 # Insert pet information to MongoDB database
@@ -99,10 +142,12 @@ def update_adv():
     return redirect(url_for('mysearch'))
       
       
+      
 # Delete the Advertisment
 @app.route('/delete_adv/<adv_id>')
 def delete_adv(adv_id):
     return render_template("deleteAdv.html",adv_id=adv_id) 
+    
     
     
 @app.route('/delete_adv2/', methods=['POST'])
@@ -110,6 +155,7 @@ def delete_adv2():
     adv_id=request.form.get('adv_id')
     mongo.db.pet.remove({'_id': ObjectId(adv_id)})
     return redirect(url_for('mysearch'))
+    
     
     
 if __name__ == '__main__':
